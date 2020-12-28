2750517fb43af5d244eae9c6c39620a5a37e13f3
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-28 09:04:28 -0500

    Draft

--- ReplaceRunningPack.gd
+++ ReplaceRunningPack.gd
@@ -6,17 +6,18 @@ func _path_to_main_pck():
 	pass
 
 func _is_editor_mode():
-	if OS.has_feature("standalone"):
-		return false
-	else:
+	if OS.has_feature("editor"):
 		return true
+	else:
+		return false
 
 func _on_WaitToRun_timeout():
-	if _is_editor_mode():
-		$Display.text = "Sorry, you need to run this outside of the editor."
-		return
+	#if _is_editor_mode():
+	#	$Display.text = "Sorry, you need to run this outside of the editor."
+	#	return
 	
-	if OS.get_executable_path().get_base_dir() == "/Applications/Godot.app/Contents/MacOS":
+	#if OS.get_executable_path().get_base_dir() == "/Applications/Godot.app/Contents/MacOS":
+	if OS.is_debug_build():
 		$Display.text = "Using the system install of Godot... "
 		var pf = File.new()
 		if pf.file_exists("test.pck"):
@@ -25,6 +26,7 @@ func _on_WaitToRun_timeout():
 			if pd.copy("test.pck", "export/main.pck") == OK:
 				$Display.text += " Success! Now let's reload our original PCK... "
 				ProjectSettings.load_resource_pack("export/main.pck")
+				get_tree().change_scene("res://ReplaceRunningPack.tscn")
 			else:
 				$Display.text += " FAILED!  We're done here."
 				return
