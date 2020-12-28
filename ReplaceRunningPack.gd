66abcc5fe50f25b1af51fd3a5907b8f92b73b65e
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-28 08:38:15 -0500

    test

--- ReplaceRunningPack.gd
+++ ReplaceRunningPack.gd
@@ -6,10 +6,13 @@ func _path_to_main_pck():
 	pass
 
 func _is_editor_mode():
-	return true
+	if OS.has_feature("standalone"):
+		return true
+	else:
+		return false
 
 func _on_WaitToRun_timeout():
 	if _is_editor_mode():
 		$Display.text = "Sorry, you need to run this outside of the editor."
 	else:
-		$Display.text = "WIP"
+		$Display.text = "WIP: executable base dir is %s" % OS.get_executable_path().get_base_dir()
