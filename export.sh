7984fec6113445a4d891cb3845414b1f2ca54cab
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-28 09:44:02 -0500

    Draft the experiment (#2)

---
+++ export.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# see https://docs.godotengine.org/en/stable/getting_started/editor/command_line_tutorial.html
+
+godot --export-pack "Mac OSX" export/main.pck
+godot --export "Mac OSX" export/Test.dmg
+cp test.pck /tmp/000-test.pck
